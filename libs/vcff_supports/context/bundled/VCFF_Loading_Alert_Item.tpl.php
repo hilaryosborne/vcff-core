c1fb37eacf2cb98fd9460a6caa3124da8bd047ad
sacrine <h.t.osborne@gmail.com>
2015-10-01 05:27:09 +0000

    Loading Alert

--- libs/vcff_supports/context/bundled/VCFF_Loading_Alert_Item.tpl.php
+++ libs/vcff_supports/context/bundled/VCFF_Loading_Alert_Item.tpl.php
@@ -1,4 +1,4 @@
-<div data-vcff-support-name="<?php echo $machine_code; ?>" class="form-loading-alert <?php echo $extra_class; ?> <?php echo $css_class; ?> <?php echo 'for-'.$display; ?><?php if (in_array('conditions',$usage_list)): ?> for-conditions<?php endif; ?><?php if (in_array('validation',$usage_list)): ?> for-validation<?php endif; ?><?php if (in_array('submission',$usage_list)): ?> for-submission<?php endif; ?><?php if (in_array('error',$usage_list)): ?> for-error<?php endif; ?>"> 
+<div data-vcff-support-name="<?php echo $machine_code; ?>" class="form-loading-alert ignore-visibility <?php echo $extra_class; ?> <?php echo $css_class; ?> <?php echo 'for-'.$display; ?><?php if (in_array('conditions',$usage_list)): ?> for-conditions<?php endif; ?><?php if (in_array('validation',$usage_list)): ?> for-validation<?php endif; ?><?php if (in_array('submission',$usage_list)): ?> for-submission<?php endif; ?><?php if (in_array('error',$usage_list)): ?> for-error<?php endif; ?>"> 
     <div class="inner-wrap">
         <?php echo $loading_msg; ?>
     </div>
